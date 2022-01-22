467e44e2a9a82d48d976978baf79ab48e5ff3735
Rogelio Rios <rogeliorios348@gmail.com>
2022-01-21 18:24:21 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Rightstuf Scrapper API
+**Author**: Rogelio Rios | **Version**: 1.0
+
+## Installation
+---
+On root folder run:
+```
+npm install
+```
+to start server:
+```
+npm run dev
+```
+
+## Description
+- - - 
+The purpose of this application is retrieve the information from the rightstuf website. By provinding API for searches, daily deals, featured products, pre-orders, and new releases.
+
+## Packages
+---
+| Package Name | Description | Version |
+| ---- | ---- | ---- |
+| express | Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.  | 4.17.2 |
+| axios | Promise based HTTP client for the browser and node.js | 0.25.0 |
---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 8080
+
+const generateScraperURL = (api_key) => `http://api.scraperapi.com?api_key=${api_key}&autoparse=true&render=true`;
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.send('<h1>Welcome to Rightstuf scrapper API</h1>')
+})
+
+// GET Search Results
+app.get('/api/search/:searchQuery', async (req, res) => {
+    const { searchQuery } = req.params;
+    const {api_key} = req.query
+
+    try{
+        //const response = await request(`${baseURL}&url=https://www.amazon.com/dp/${productId}`);
+        if(api_key === undefined){
+            res.json({
+                message: "ERROR: Please enter api key in URL",
+                status: 404
+            })
+        } else {
+            const response = await axios.get(`https://www.rightstufanime.com/api/items?c=546372&country=US&currency=USD&fieldset=search&include=facets&language=en&limit=12&n=2&offset=0&pricelevel=5&q=${searchQuery}&sort=relevance%3Aasc&use_pcv=F`)
+            //const response = await axios.get(`${generateScraperURL(api_key)}&url=https://www.rightstufanime.com/search?show=96&keywords=${searchQuery}`);
+            const items = response.data.items
+            const array = []
+            items.map(item => {
+                //variables that output data depending on the conditions
+                let tempRating = (item) =>{
+                    if(item.custitem_ns_pr_rating == undefined || item.custitem_ns_pr_rating === null || item.custitem_ns_pr_rating === 0){
+                        return ''
+                    } else{
+                        return item.custitem_ns_pr_rating
+                    }
+
+                }
+                let tempReleaseTag = (item) => {
+                    if(item.custitem_rs_new_releases_preorders === undefined || item.custitem_rs_new_releases_preorders == null){
+                        return ''
+                    } else {
+                        return item.custitem_rs_new_releases_preorders
+                    }
+                }
+                //pushes JSON objects to empty array
+                array.push({
+                    name: item.storedisplayname,
+                    type: item.custitem_rs_web_class,
+                    genre: item.custitem_rs_genre,
+                    sku: item.upccode,
+                    description: item.storedescription,
+                    img_URL: `https://www.rightstufanime.com/images/productImages/${item.upccode}_${item.custitem_rs_web_class}-${item.urlcomponent}-primary.jpg`,
+                    item_url: `https://www.rightstufanime.com/${item.urlcomponent}`,
+                    release_date: item.custitem_rs_release_date,
+                    rating: tempRating(item),
+                    msrp: item.pricelevel1,
+                    salePrice: item.pricelevel2,
+                    currentPrice: item.onlinecustomerprice,
+                    ispurchasable: item.ispurchasable,
+                    instock: item.isinstock,
+                    backorder: item.custitem_rs_publisher_backorder,
+                    top_selling: item.custitem_rs_top_selling_item,
+                    purchaseable: item.custitem_rs_availabe_for_purchase,
+                    release_tag: tempReleaseTag(item),
+                    outofstock_msg: item.outofstockmessage,
+                    stock_msg: item.stockdescription,
+                })
+            })
+            console.log(array)
+            //res.json(response.data.items)
+            res.json(array)
+        }
+
+    } catch(error){
+        res.json(error)
+    }
+})
+
+app.listen(PORT,() =>{
+    console.log(`Server is running on PORT ${PORT}`)
+});
